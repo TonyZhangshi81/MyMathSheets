ee92fb6c228500feec4e1f153e0c621a390edb43
zhangcg <zhangcg@ZHANGCG-PC>
2018-09-26 18:29:06 +0800

    射門得分題型（初步構思）

---
+++ ComputationalStrategy/Item/ScoreGoalFormula.cs
@@ -0,0 +1,25 @@
+﻿using MyMathSheets.CommonLib.Main.Item;
+using System.Collections.Generic;
+
+namespace MyMathSheets.ComputationalStrategy.Item
+{
+	/// <summary>
+	/// 射門得分
+	/// </summary>
+	public class ScoreGoalFormula
+	{
+		/// <summary>
+		/// 球類计算式集合
+		/// </summary>
+		public IList<Formula> FruitsFormulas { get; set; }
+
+		/// <summary>
+		/// 球門容器计算式集合（只有兩個球門0或者1使用）
+		/// </summary>
+		public IList<Formula> ContainersFormulas { get; set; }
+
+		/// <summary>
+		/// </summary>
+		public IList<int> Seats { get; set; }
+	}
+}
