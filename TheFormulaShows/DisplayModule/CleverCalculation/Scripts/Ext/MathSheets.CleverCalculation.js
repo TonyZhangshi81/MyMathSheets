966553a65f09b359e6a823742cad9cd75bf29c0a
zhangchenggang <tonyzhangshi@163.com>
2020-07-24 22:37:25 +0800

    add CleverCalculation displaymodule

---
+++ TheFormulaShows/DisplayModule/CleverCalculation/Scripts/Ext/MathSheets.CleverCalculation.js
@@ -0,0 +1,109 @@
+﻿var MathSheets = MathSheets || {};
+
+MathSheets.CleverCalculation = MathSheets.CleverCalculation || (function () {
+    // 打印設置
+    printSetting = function () {
+        $("input[id*='inputAc']").each(function (index, element) {
+            $(element).addClass('input-print');
+            $(element).removeAttr('placeholder');
+            $(element).removeAttr("disabled");
+        });
+    },
+
+        // 打印后頁面設定
+        printAfterSetting = function () {
+            $("input[id*='inputAc']").each(function (index, element) {
+                $(element).removeClass('input-print');
+                $(element).attr('placeholder', '??');
+                $(element).attr("disabled", "disabled");
+            });
+        },
+
+        // 當光標落在文本輸入框中的時候發生的事件
+        inputOnFocus = function (e, index) {
+            var dialogueAry = ($("#hiddenAcTony").val() || '').split(';');
+            MathSheets.HelloMrTony.setDialogueArray("<h5>" + dialogueAry[index] + "</h5>");
+            MathSheets.HelloMrTony.help();
+        },
+
+        // 答题验证(正确:true  错误:false)
+        _CleverCalculationCorrecting = function (index, element) {
+            // 解密
+            var answer = $.base64.atob($('#hiddenAc' + index).val(), true);
+            // 验证输入值是否与答案一致(并且特殊情况下,答案值可以是任意值,此处以-999代替)
+            if ($(element).val() == answer
+                || (parseInt(answer) == -999 && $(element).val() != '')) {
+                // 动错题集中移除当前项目
+                removeInputElementArray({ position: "mathSheetCleverCalculation", id: $(element).attr("id") });
+
+                // 对错图片显示和隐藏
+                $('#imgOKCleverCalculation' + index).show();
+                $('#imgNoCleverCalculation' + index).hide();
+                // 移除圖片抖動特效
+                $('#imgNoCleverCalculation' + index).removeClass("shake shake-slow");
+                $(element).attr("disabled", "disabled");
+                // 正确:true
+                return true;
+            } else {
+                // 对错图片显示和隐藏
+                $('#imgOKCleverCalculation' + index).hide();
+                $('#imgNoCleverCalculation' + index).show();
+                $('#imgNoCleverCalculation' + index).animate({
+                    width: "40px",
+                    height: "40px",
+                    marginLeft: "0px",
+                    marginTop: "0px"
+                }, 1000, function () {
+                    // 添加圖片抖動特效（只針對錯題）
+                    $(this).addClass("shake shake-slow");
+                });
+                // 错误:false
+                return false;
+            }
+        },
+
+        // 设定页面所有输入域为可用状态(巧算)
+        ready = function () {
+            $("input[id*='inputAc']").each(function (index, element) {
+                // 收集所有可輸入項目ID
+                __allInputElementArray.push({ position: "mathSheetCleverCalculation", id: $(element).attr("id") });
+
+                $(element).removeAttr("disabled");
+            });
+        },
+
+        // 订正(巧算题)
+        makeCorrections = function () {
+            var fault = 0;
+            $("input[id*='inputAc']").each(function (index, element) {
+                // 答题验证
+                if (!_CleverCalculationCorrecting(index.toString().PadLeft(2, '0'), element)) {
+                    // 答题错误时,错误件数加一
+                    fault++;
+                }
+            });
+            return fault;
+        },
+
+        // 巧算交卷
+        theirPapers = function () {
+            $("input[id*='inputAc']").each(function (index, element) {
+                // 答题验证
+                if (!_CleverCalculationCorrecting(index.toString().PadLeft(2, '0'), element)) {
+                    // 答题错误时,错误件数加一
+                    __isFault++;
+                } else {
+                    __isRight++;
+                }
+            });
+        };
+
+    return {
+        printSetting: printSetting,
+        printAfterSetting: printAfterSetting,
+        ready: ready,
+        inputOnFocus: inputOnFocus,
+        makeCorrections: makeCorrections,
+        theirPapers: theirPapers
+    };
+}());
---
+++ TheFormulaShows/DisplayModule/CleverCalculation/Support/CleverCalculationHtmlSupport.cs
@@ -0,0 +1,159 @@
+﻿using MyMathSheets.CommonLib.Main.HtmlSupport;
+using MyMathSheets.CommonLib.Main.HtmlSupport.Attributes;
+using MyMathSheets.CommonLib.Main.VirtualHelper;
+using MyMathSheets.CommonLib.Util;
+using MyMathSheets.CommonLib.Util.Security;
+using MyMathSheets.ComputationalStrategy.CleverCalculation.Item;
+using MyMathSheets.ComputationalStrategy.CleverCalculation.Main.Parameters;
+using System.Text;
+
+namespace MyMathSheets.TheFormulaShows.CleverCalculation.Support
+{
+	/// <summary>
+	/// 題型模板支援類
+	/// </summary>
+	[HtmlSupport("CleverCalculation")]
+	[Substitute(SubstituteType.Script, "<script src=\"../Scripts/Ext/MathSheets.CleverCalculation.js\" charset=\"utf-8\"></script>")]
+	[Substitute(SubstituteType.ReadyEvent, "MathSheets.CleverCalculation.ready();")]
+	[Substitute(SubstituteType.MakeCorrectionsEvent, "fault += MathSheets.CleverCalculation.makeCorrections();")]
+	[Substitute(SubstituteType.TheirPapersEvent, "MathSheets.CleverCalculation.theirPapers();")]
+	[Substitute(SubstituteType.PrintSettingEvent, "MathSheets.CleverCalculation.printSetting();")]
+	[Substitute(SubstituteType.PrintAfterSettingEvent, "MathSheets.CleverCalculation.printAfterSetting();")]
+	public class CleverCalculationHtmlSupport : HtmlSupportBase<CleverCalculationParameter>
+	{
+		/// <summary>
+		/// 智能提示
+		/// </summary>
+		private HelperDialogue BrainpowerHint { get; set; }
+
+		private int _brainpowerIndex;
+
+		/// <summary>
+		/// 題型HTML模板作成
+		/// </summary>
+		/// <param name="p">題型參數</param>
+		/// <returns>題型HTML模板信息</returns>
+		public override string MakeHtmlContent(CleverCalculationParameter p)
+		{
+			if (p.Formulas.Count == 0)
+			{
+				return string.Empty;
+			}
+
+			int numberOfColumns = 0;
+			bool isRowHtmlClosed = false;
+			BrainpowerHint = p.BrainpowerHint;
+			_brainpowerIndex = 0;
+
+			int controlIndex = 0;
+			StringBuilder html = new StringBuilder();
+			StringBuilder rowHtml = new StringBuilder();
+			StringBuilder colHtml = new StringBuilder();
+			foreach (CleverCalculationFormula item in p.Formulas)
+			{
+				isRowHtmlClosed = false;
+				colHtml.AppendLine("<div class=\"col-md-4 form-inline\">");
+				colHtml.AppendLine("<h5>");
+
+				// TODO
+
+				colHtml.AppendLine("</h5>");
+				colHtml.AppendLine("<div class=\"divCorrectOrFault-1\">");
+				colHtml.AppendLine(string.Format("<img id=\"imgOKCleverCalculation{0}\" src=\"../Content/image/correct.png\" class=\"imgCorrect-1\" />", controlIndex.ToString().PadLeft(2, '0')));
+				colHtml.AppendLine(string.Format("<img id=\"imgNoCleverCalculation{0}\" src=\"../Content/image/fault.png\" class=\"imgFault-1\" />", controlIndex.ToString().PadLeft(2, '0')));
+				colHtml.AppendLine("</div>");
+				colHtml.AppendLine("</div>");
+
+				controlIndex++;
+				numberOfColumns++;
+
+				// 單位行上顯示3道題目
+				if (numberOfColumns == 3)
+				{
+					rowHtml.AppendLine("<div class=\"row text-center row-margin-top\">");
+					rowHtml.Append(colHtml.ToString());
+					rowHtml.AppendLine("</div>");
+
+					html.Append(rowHtml);
+
+					isRowHtmlClosed = true;
+					numberOfColumns = 0;
+					rowHtml.Length = 0;
+					colHtml.Length = 0;
+				}
+			}
+
+			if (!isRowHtmlClosed)
+			{
+				rowHtml.AppendLine("<div class=\"row text-center row-margin-top\">");
+				rowHtml.Append(colHtml.ToString());
+				rowHtml.AppendLine("</div>");
+
+				html.Append(rowHtml);
+			}
+
+			if (html.Length != 0)
+			{
+				html.Insert(0, "<div class=\"div-page-content\">").AppendLine();
+				// 會話提示內容保存至畫面
+				//html.AppendLine(string.Format(DIALOGUE_CONTENT_HTML_FORMAT, GetCleverCalculationDialogue()));
+				html.AppendLine().Append("</div>");
+				//html.Insert(0, string.Format(PAGE_HEADER_HTML_FORMAT, "CleverCalculation", "巧算"));
+			}
+
+			return html.ToString();
+		}
+
+		/// <summary>
+		/// 會話提示內容保存至畫面
+		/// </summary>
+		/// <returns>HTML模板信息</returns>
+		private string GetCleverCalculationDialogue()
+		{
+			if (BrainpowerHint == null)
+			{
+				return string.Empty;
+			}
+
+			StringBuilder html = new StringBuilder();
+			BrainpowerHint.Dialogues.ForEach(d =>
+			{
+				html.AppendFormat("{0};", d);
+			});
+			html.Length -= 1;
+
+			return html.ToString();
+		}
+
+		/// <summary>
+		/// 計算式HTML作成
+		/// </summary>
+		/// <param name="item">填空項目</param>
+		/// <param name="parameter">計算式中的數值</param>
+		/// <param name="gap">當前顯示項目所在計算式中的位置</param>
+		/// <param name="index">控件索引號</param>
+		/// <returns>HTML模板信息</returns>
+		private string GetHtml(GapFilling item, int parameter, GapFilling gap, int index)
+		{
+			StringBuilder html = new StringBuilder();
+			if (item == gap)
+			{
+				if (BrainpowerHint.FormulaIndex.Count > _brainpowerIndex && BrainpowerHint.FormulaIndex[_brainpowerIndex] == index)
+				{
+					//html.AppendFormat(INPUT_HTML_FORMAT, index.ToString().PadLeft(2, '0'), string.Format(DIALOGUE_JS_HTML_FORMAT, _brainpowerIndex++));
+				}
+				else
+				{
+					//html.AppendFormat(INPUT_HTML_FORMAT, index.ToString().PadLeft(2, '0'), string.Empty);
+				}
+				//html.AppendFormat(ANSWER_HTML_FORMAT, index.ToString().PadLeft(2, '0'), Base64.EncodeBase64(parameter.ToString()));
+			}
+			else
+			{
+				//html.AppendFormat(LABEL_HTML_FORMAT, parameter);
+			}
+
+			return html.ToString();
+		}
+	}
+}
