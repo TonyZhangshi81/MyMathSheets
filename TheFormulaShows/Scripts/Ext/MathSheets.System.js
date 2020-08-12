2b39f25c32e8c783411b340b2df395be87e4ae0f
TonyZhangshi81 <tonyzhangshi@163.com>
2020-08-12 16:43:06 +0800

    add MathSheets.System.js

---
+++ TheFormulaShows/Scripts/Ext/MathSheets.System.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    console.log(navigator.userAgent);
+
+    var os = function () {
+        var ua = navigator.userAgent,
+            isWindowsPhone = /(?:Windows Phone)/.test(ua),
+            isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+            isAndroid = /(?:Android)/.test(ua),
+            isFireFox = /(?:Firefox)/.test(ua),
+            isChrome = /(?:Chrome|CriOS)/.test(ua),
+            isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+            isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+            isPc = !isPhone && !isAndroid && !isSymbian;
+        return {
+            isWindowsPhone: isWindowsPhone,
+            isSymbian: isSymbian,
+            isAndroid: isAndroid,
+            isFireFox: isFireFox,
+            isChrome: isChrome,
+            isTablet: isTablet,
+            isPhone: isPhone,
+            isPc: isPc
+        };
+    }();
+
+    $(window).on("orientationchange", function (event) {
+        console.log(window.orientation);
+    });
+});
