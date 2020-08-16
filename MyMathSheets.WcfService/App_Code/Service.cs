04a83879ae8500fa3aa060e33ac4581a546c881c
zhangchenggang <tonyzhangshi@163.com>
2020-08-16 16:55:04 +0800

    add wcf

---
+++ MyMathSheets.WcfService/App_Code/Service.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
+
+namespace MyMathSheets.WcfService
+{
+	/// <summary>
+	/// </summary>
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+	public class Service : IService
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="parameter"> </param>
+		/// <returns> </returns>
+		public string MarkFormula(TopicParamater parameter)
+		{
+			return "http://localhost:8080/AppData/Work/Page/20200814205554998.html";
+		}
+	}
+}
